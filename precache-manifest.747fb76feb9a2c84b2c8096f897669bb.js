adf2c2942ce433f0b34165063e1bcaf96dfa2949
Diego Mena <diego.mena@unad.edu.co>
2023-05-21 14:36:51 -0500

    website in progress

---
+++ precache-manifest.747fb76feb9a2c84b2c8096f897669bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcc708c03a19a87bd78212c67fc1aaa",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "0278dc1f8764a3630a0c",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "19a56058e0637fa2fd8e",
+    "url": "@yield('content-viewer-url')/static/css/main.218a287c.chunk.css"
+  },
+  {
+    "revision": "0278dc1f8764a3630a0c",
+    "url": "@yield('content-viewer-url')/static/js/2.f48ae99b.chunk.js"
+  },
+  {
+    "revision": "73a2cb6665b648320b93dbc14dbfa5c3",
+    "url": "@yield('content-viewer-url')/static/js/2.f48ae99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a56058e0637fa2fd8e",
+    "url": "@yield('content-viewer-url')/static/js/main.60cb0829.chunk.js"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
